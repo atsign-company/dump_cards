2e3a3661317c5ad1984577bde484a991ff5e3ca3
Chris Swan <478926+cpswan@users.noreply.github.com>
2021-06-15 10:20:54 +0100

    Warn for cards that aren't issues

--- dump_project_cards.py
+++ dump_project_cards.py
@@ -5,21 +5,20 @@ import os, sys, json, requests, yaml
 
 # Color constants
 # Reference: https://gist.github.com/chrisopedia/8754917
+COLERR="\033[0;31m"
 COLINFO="\033[0;35m"
 COLRESET="\033[m"
 
 baseurl = 'https://api.github.com'
 headers = {"Content-Type": "application/json", "Accept": "application/vnd.github.inertia-preview+json"}
 
-if len(sys.argv) != 2:
-    #print("   Usage: " + sys.argv[0] + " myproject-id org_name")
-    print("   Usage: " + sys.argv[0] + " myproject-id")
+if len(sys.argv) != 3:
+    print("   Usage: " + sys.argv[0] + " myproject-id org_name")
     sys.exit(1)
 
 project_id = sys.argv[1]
 cards_file = project_id + ".csv"
-#org = sys.argv[2]
-org = "atsign-foundation"
+org = sys.argv[2]
 token = os.environ['GITHUB_API_TOKEN']
 
 def list_project_cards():
@@ -29,8 +28,8 @@ def list_project_cards():
         auth=(org, token))
     if response.status_code != 200:
         # An error occured
-        print(COLINFO + "Error getting project columns : " + str(response.status_code) + " " + response.text
-        + COLRESET)
+        print(COLERR + "Error getting project columns : ",
+            + str(response.status_code) + " " + response.text + COLRESET)
 
     # Convert repos to YAML
     json_cards = json.loads(response.text)
@@ -39,14 +38,18 @@ def list_project_cards():
     f.write("Issue Key,Summary,Description,Acceptance Criteria,Story Points\n")
     for card in json_cards:
         if not(card["archived"]):
-            issues = requests.get(card["content_url"], 
-                headers=headers, 
-                auth=(org, token))
-            json_issues = [json.loads(issues.text)]
-            #f.write (f'{json.dumps(json_issue, indent=4, sort_keys=True)}\n')
-            for issue in json_issues:
-                f.write (f'{issue["number"]},')
-                f.write (f'{issue["title"]},,,\n')
+            try:
+                issues = requests.get(card["content_url"], 
+                    headers=headers, 
+                    auth=(org, token))
+                json_issues = [json.loads(issues.text)]
+                #f.write (f'{json.dumps(json_issue, indent=4, sort_keys=True)}\n')
+                for issue in json_issues:
+                    f.write (f'{issue["number"]},')
+                    f.write (f'{issue["title"]},,,\n')
+            except(KeyError):
+                print(COLINFO + "Card found that hasn't been converted to an issue:",
+                    COLRESET + f'{card["note"]}')
     f.close
 
 list_project_cards()
