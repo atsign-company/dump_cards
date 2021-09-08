a87369e78d328c8e8deb958381f26cfb110aacd7
Chris Swan <478926+cpswan@users.noreply.github.com>
2021-09-08 12:12:14 +0100

    Just work on .csv files

--- merge.sh
+++ merge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Merge together the last two files, stripping the header line from the second
-LATEST_FILES=$(ls -tr | tail -2)
-FIRST_FILE=$(echo "$LATEST_FILES" | sed 's/[^ ]* //')
-SECOND_FILE=$(echo "$LATEST_FILES" | sed 's/ [^ ]*//')
+LATEST_FILES=$(ls -tr *.csv | tail -2)
+FIRST_FILE=$(echo $LATEST_FILES | sed 's/[^ ]* //')
+SECOND_FILE=$(echo $LATEST_FILES | sed 's/ [^ ]*//')
 cat "$FIRST_FILE" > "$FIRST_FILE.$SECOND_FILE"
 tail -n+2 "$SECOND_FILE" >> "$FIRST_FILE.$SECOND_FILE"
