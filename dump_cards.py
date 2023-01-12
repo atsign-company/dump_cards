bd823485d057ea53148b17109db8b9a5fd5c3b2d
xavierchanth <xchanthavong@gmail.com>
2023-01-12 17:14:39 -0500

    chore: Replace dump_cards.py with dump_cards_v2.py

--- dump_cards.py
+++ dump_cards.py
@@ -2,8 +2,8 @@
 # pip3 install InquirerPy
 import base64, sys
 from InquirerPy import inquirer
-from atdumpmemex import *
-from atdumpcards import * 
+from atdumpcardsv2 import *
+from atdumpcards import *
 
 # Create empty lists for later appends
 orglist = []
@@ -26,13 +26,13 @@ json_projects = list_projects(org)
 for project in json_projects:
     pchoice.append(str(project["number"])+' '+str(project["id"])
         +' '+project["name"])
-#Get beta projects from API
-json_bprojects = list_memex_projects(org)
-for node in json_bprojects["data"]["organization"]["projectsNext"]["nodes"]:
+#Get V2 projects from API
+json_bprojects = list_v2_projects(org)
+for node in json_bprojects["data"]["organization"]["projectsV2"]["nodes"]:
     #proj_num = str(node["number"])
     proj_id = node["id"]
     if not node["closed"]:
-        pchoice.append('b '+proj_id+' '+node["title"])
+        pchoice.append('v2 '+proj_id+' '+node["title"])
 if not pchoice:
     print("No projects found")
     sys.exit()
@@ -40,16 +40,25 @@ if not pchoice:
 pselect = inquirer.select(message="Which project?",
     choices=pchoice).execute()
 project_id = pselect.partition(" ")[2].partition(" ")[0]
-# Beta projects
-if pselect.partition(" ")[0] == 'b':
-    json_columns = list_memex_columns(project_id)
-    if not json_columns:
-        print("No columns found")
+# V2 projects
+if pselect.partition(" ")[0] == 'v2':
+    json_fields = list_v2_fields(project_id)
+    if not json_fields:
+        print("No fields found")
         sys.exit()
-    column_id = inquirer.select(message="Which column?",
-       choices=json_columns).execute().partition(" ")[0]
-    list_memex_cards(column_id,project_id)
-    print("Exported: "+column_id+".csv")
+    json_columns = list_v2_fields(project_id)
+    field = inquirer.select(message="Which field?",
+      choices=json_fields).execute()
+    field_id = field.partition(" ")[0]
+    field_name = field.partition(" ")[1]
+    json_columns = list_v2_columns(field_id)
+    if not json_columns:
+      print("No columns found")
+      sys.exit()
+    column_name = inquirer.select(message="Which column?",
+       choices=json_columns).execute()
+    list_v2_cards(field_id,project_id,column_name)
+    print("Exported: "+column_name+".csv")
 else:
 # Regular projects
     json_columns = list_project_columns(project_id)
